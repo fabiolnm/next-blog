d3f168823b1a055d8ea7c385dbf961309e5719a5
Fábio Luiz Nery de Miranda <fabio@7gen.com>
2024-02-10 11:03:21 -0800

    Configured next-mdx
    https://nextjs.org/docs/app/building-your-application/configuring/mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
+import createMdx from '@next/mdx'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // https://nextjs.org/docs/messages/next-image-unconfigured-host
   images: {
     domains: ['source.unsplash.com'],
   },
-};
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['mdx', 'ts', 'tsx'],
+}
 
-export default nextConfig;
+const withMDX = createMdx({
+  // Add markdown plugins here, as desired
+  options: {
+  }
+})
+
+export default withMDX(nextConfig)
