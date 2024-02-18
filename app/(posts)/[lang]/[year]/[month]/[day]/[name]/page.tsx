a86241e88443fb0f0fcf37a61b295e57d318b198
Fábio Luiz Nery de Miranda <fabio@7gen.com>
2024-02-17 22:45:26 -0800

    Render 'Not Found' if post translation is not available

--- app/(posts)/[lang]/[year]/[month]/[day]/[name]/page.tsx
+++ app/(posts)/[lang]/[year]/[month]/[day]/[name]/page.tsx
@@ -7,29 +7,30 @@ import Blog from '@/app/blog/Blog'
 export async function generateMetadata({ params }: { params: any }) {
   const { lang, year, month, day, name } = params
 
-  const { metadata } = await
-    import(`../../../../../${lang}/${year}/${month}/${day}/${name}/metadata`)
+  try {
+    const { metadata } = await
+      import(`../../../../../${lang}/${year}/${month}/${day}/${name}/metadata`)
 
-  const description = metadata.description
-    .replace(/<[^>]*>/g, '').replace(/\s+/g, ' ')
+    const description = metadata.description
+      .replace(/<[^>]*>/g, '').replace(/\s+/g, ' ')
 
-  return { ...metadata, description }
+    return { ...metadata, description }
+  } catch (error) {
+    return { title: 'Not found', description: '' }
+  }
 }
 
 export default async function Page ({ params }: { params: any }) {
   const { lang, year, month, day, name } = params
 
-  const { metadata } = await
-    import(`../../../../../${lang}/${year}/${month}/${day}/${name}/metadata`)
+  const { title, description } = await generateMetadata({ params })
 
   const Body = await import(
     `../../../../../${lang}/${year}/${month}/${day}/${name}/body.mdx`
-  ).then((module) => module.default)
-
-  const { title, description } = metadata
+  ).then((module) => module.default).catch(() => null)
 
   return (
-    <Blog>
+    <Blog params={params}>
       <Paper sx={{ px: 2, py: 0.5 }}
         className={`${flavor.github} ${page.github}`}
       >
@@ -37,11 +38,15 @@ export default async function Page ({ params }: { params: any }) {
           <Typography variant="h6" component="h1" gutterBottom>
             {title}
           </Typography>
-          <small>
-            <em>{ new Date(+year, +month - 1, +day).toDateString() }</em>
-          </small>
-          <p dangerouslySetInnerHTML={{ __html: description }} />
-          <Body />
+          { Body && (
+            <>
+              <small>
+                <em>{ new Date(+year, +month - 1, +day).toDateString() }</em>
+              </small>
+              <p dangerouslySetInnerHTML={{ __html: description }} />
+              <Body />
+            </>
+          )}
         </article>
       </Paper>
     </Blog>
